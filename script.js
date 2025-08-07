7f5511406c8ff5cadaac83af82b63f9c33949d2a
DestinySolomon <deesystems247@gmail.com>
2025-08-07 16:08:07 +0100

    added login page and styled it

--- script.js
+++ script.js
@@ -147,9 +147,23 @@ userInput.addEventListener('keypress', e => {
   });
 
 
-  // Registration Form
+  // Login Form
 
-  
+  function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    const toggleIcon = document.querySelector(".toggle-password");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      toggleIcon.textContent = "🙈";
+    } else {
+      passwordInput.type = "password";
+      toggleIcon.textContent = "👁️";
+    }
+  }
+
+
+
+
 
 
 // Footer
